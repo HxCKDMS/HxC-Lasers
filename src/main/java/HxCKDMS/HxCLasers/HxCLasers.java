987ca2b7041a4cb588e35e23a6e45078061f5b48
karelmikie3 <karelkling1@gmail.com>
2015-06-13 17:23:49 +0200

    setting up

---
+++ src/main/java/HxCKDMS/HxCLasers/HxCLasers.java
@@ -0,0 +1,23 @@
+package HxCKDMS.HxCLasers;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+import static HxCKDMS.HxCLasers.Lib.References.*;
+
+@Mod(modid = MOD_ID, name = MOD_NAME, version = VERSION, dependencies = DEPENDENCIES)
+public class HxCLasers {
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
